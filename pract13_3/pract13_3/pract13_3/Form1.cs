964cfe24af0c9d5e0e73c5e9f202a3a3fa382744
levriavkin <64539455+levriavkin@users.noreply.github.com>
2023-03-21 15:42:43 +0500

    Add files via upload

---
+++ pract13_3/pract13_3/pract13_3/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using pract13_3;
+namespace prakt13_2
+{
+    public partial class Form1 : Form
+    {
+        //SortedList<string,string> kostumelist = new SortedList<string,string>();
+        private IList<Kostume> kostumelist = new List<Kostume>();
+
+        private void addKostume(string name, int size, string color,string pol,int price,string Sizexx)
+        {
+            Kostume s = new Kostume(name, size,color,pol,price,Sizexx);
+            
+            kostumelist.Add( s);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = s.Sizexx().ToString();
+            showListInGrid();
+        }
+        //Удаление студента с колекции
+        private void deleteStudent(int elementIndex)
+        {
+            kostumelist.RemoveAt(elementIndex);
+            showListInGrid();
+        }
+        //Отображение колекции в таблице
+        private void showListInGrid()
+        {
+            //ICollection<string> keys = kostumelist.Keys;
+            dataGridView1.Rows.Clear();
+            foreach (Kostume s in kostumelist)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                DataGridViewTextBoxCell cell1 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell2 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell3 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell4 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell5 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell6 = new
+                DataGridViewTextBoxCell();
+
+                cell1.ValueType = typeof(string);
+                cell1.Value = s.getName();
+                cell2.ValueType = typeof(int);
+                cell2.Value = s.getSize();
+                cell3.ValueType = typeof(string);
+                cell3.Value = s.getColor();
+                cell4.ValueType = typeof(string);
+                cell4.Value = s.getPol();
+                cell5.ValueType = typeof(int);
+                cell5.Value = s.getPrice();
+                cell6.ValueType = typeof(int);
+                cell6.Value = s.Sizexx();
+                row.Cells.Add(cell1);
+                row.Cells.Add(cell2);
+                row.Cells.Add(cell3);
+                row.Cells.Add(cell4);
+                row.Cells.Add(cell5);
+                row.Cells.Add(cell6);
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+
+        private DataGridViewColumn dataGridViewColumn1 = null;
+        private DataGridViewColumn dataGridViewColumn2 = null;
+        private DataGridViewColumn dataGridViewColumn3 = null;
+        private DataGridViewColumn dataGridViewColumn4 = null;
+        private DataGridViewColumn dataGridViewColumn5 = null;
+        private DataGridViewColumn dataGridViewColumn6 = null;
+
+        private void initDataGridView()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Columns.Add(getDataGridViewColumn1());
+            dataGridView1.Columns.Add(getDataGridViewColumn2());
+            dataGridView1.Columns.Add(getDataGridViewColumn3());
+            dataGridView1.Columns.Add(getDataGridViewColumn4());
+            dataGridView1.Columns.Add(getDataGridViewColumn5());
+            dataGridView1.Columns.Add(getDataGridViewColumn6());
+            dataGridView1.AutoResizeColumns();
+        }
+        //Динамическое создание первой колонки в таблице
+        private DataGridViewColumn getDataGridViewColumn1()
+        {
+            if (dataGridViewColumn1 == null)
+            {
+                dataGridViewColumn1 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn1.Name = "";
+                dataGridViewColumn1.HeaderText = "Имя";
+                dataGridViewColumn1.ValueType = typeof(string);
+                dataGridViewColumn1.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn1;
+        }
+        //Динамическое создание второй колонки в таблице
+        private DataGridViewColumn getDataGridViewColumn2()
+        {
+            if (dataGridViewColumn2 == null)
+            {
+                dataGridViewColumn2 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn2.Name = "";
+                dataGridViewColumn2.HeaderText = "Размер";
+                dataGridViewColumn2.ValueType = typeof(int);
+                dataGridViewColumn2.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn2;
+        }
+        //Динамическое создание третей колонки в таблице
+        private DataGridViewColumn getDataGridViewColumn3()
+        {
+            if (dataGridViewColumn3 == null)
+            {
+                dataGridViewColumn3 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn3.Name = "";
+                dataGridViewColumn3.HeaderText = "Цвет";
+                dataGridViewColumn3.ValueType = typeof(string);
+                dataGridViewColumn3.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn3;
+        }
+        private DataGridViewColumn getDataGridViewColumn4()
+        {
+            if (dataGridViewColumn4 == null)
+            {
+                dataGridViewColumn4 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn4.Name = "";
+                dataGridViewColumn4.HeaderText = "Пол";
+                dataGridViewColumn4.ValueType = typeof(string);
+                dataGridViewColumn4.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn4;
+        }
+        //Динамическое создание третей колонки в таблице
+        private DataGridViewColumn getDataGridViewColumn5()
+        {
+            if (dataGridViewColumn5 == null)
+            {
+                dataGridViewColumn5 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn5.Name = "";
+                dataGridViewColumn5.HeaderText = "Цена";
+                dataGridViewColumn5.ValueType = typeof(int);
+                dataGridViewColumn5.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn5;
+        }
+        private DataGridViewColumn getDataGridViewColumn6()
+        {
+            if (dataGridViewColumn6 == null)
+            {
+                dataGridViewColumn6 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn6.Name = "";
+                dataGridViewColumn6.HeaderText = "Метод";
+                dataGridViewColumn6.ValueType = typeof(int);
+                dataGridViewColumn6.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn6;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            initDataGridView();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addKostume(textBox1.Text, Convert.ToInt32(textBox2.Text), textBox3.Text, textBox4.Text, Convert.ToInt32(textBox5.Text),textBox6.Text);
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int selectedRow = dataGridView1.SelectedCells[0].RowIndex;
+            DialogResult dr = MessageBox.Show("Удалить строку?", "",MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                try
+                {
+                    deleteStudent(selectedRow);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ pract13_3/pract13_3/pract13_3/Kostume.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract13_3
+{
+    class Kostume
+    {
+        string info = "";
+        private string name;
+        private int size;
+        private string color;
+        private string pol;
+        private int price;
+        private string Sizex;
+        public Kostume(string name, int size, string color,string pol,int price,string Sizexx)
+        {
+            this.name = name;
+            this.size = size;
+            this.color = color;
+            this.pol = pol;
+            this.price = price;
+            this.Sizex = Sizexx;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+        public int getSize()
+        {
+            return this.size;
+        }
+        public string getColor()
+        {
+            return this.color;
+        }
+        public string getPol()
+        {
+            return this.pol;
+        }
+        public int getPrice()
+        {
+            return this.price;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public void setSize(int size)
+        {
+            this.size = size;
+        }
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+        public void setPol(string pol)
+        {
+            this.pol= pol;
+        }
+        public void setPrice(int price)
+        {
+            this.price = price;
+        }
+        public void setSizex(string Sizexx)
+        {
+            this.Sizex = Sizexx;
+        }
+        public string getSizex()
+        {
+            return this.Sizex;
+        }
+        internal object Sizexx()
+        {if (size > 40)
+            {
+                info = "Взрослый";
+            }
+            else info = "Ребенок";
+            return info;
+        }
+    }
+}
